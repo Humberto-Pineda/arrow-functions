21d79f96b7afb56d0795f7dac4ffaaf068b202f1
Humberto Pineda <bpineda7540@gmail.com>
2022-08-16 02:06:28 -0700

    on line 129

--- app.js
+++ app.js
@@ -108,34 +108,32 @@ console.log('Hello from the new object function', newObject(['hi', 'hello', 'are
 
 
 
-let sum = function(a, b, c, d) {
+let sum = (a, b, c, d) => {
   return a + b + c + d;
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(sum(1, 2, 3, 4));
+console.log(sum(1, 2, 3, 4));
 
 
-let objectLit = function() {
-  return {
+let objectLit = value => ({
     key1: 'value1',
     key2: 'value2',
     key3: 'value3',
-  };
-};
+  });
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(objectLit());
+console.log(objectLit());
 
 
-let sumAndProduct = function(a, b) {
-  let sum = a + b;
-  let product = a * b;
-  return [sum, product];
-};
+let sumAndProduct = (a, b) => sum(`${a} + ${b} = ${a + b}`), product(`${a} * ${b} = ${a * b}`);
+//   let sum = a + b;
+//   let product = a * b;
+//   return [sum, product];
+// };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(sumAndProduct(3, 9));
+console.log(sumAndProduct(3, 9));
 
 
 let message = function(name) {
